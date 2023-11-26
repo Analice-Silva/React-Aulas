d366c9929b318ed346d1e2bd7e36e3c8ac7efc05
Analice-Silva <analicesilvadesouuza200@gmail.com>
2023-11-26 17:26:59 -0300

    iniciando

---
+++ 1 - Módulo Front-End/1 - Período/2 - Componentes React e Props/forecast-app/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import CardContainer from './components/CardContainer';
+import Header from './components/Header';
+
+function App() {
+
+  return (
+    <>
+      <CardContainer/>
+      <Header/>
+    </>
+  )
+}
+
+export default App;
---
+++ 1 - Módulo Front-End/1 - Período/2 - Componentes React e Props/forecast-app/src/components/CardContainer.tsx
@@ -0,0 +1,12 @@
+
+function CardContainer() {
+  return (
+    <>
+        <div>
+            <p>Aqui é CardContainer</p>
+        </div>
+    </>
+  )
+}
+
+export default CardContainer;
